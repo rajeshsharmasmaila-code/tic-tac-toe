931db34c2838368fda1fb13dd882c5a45ef37da2
rajeshsharmasmaila-code <rajeshsharma.smaila@gmail.com>
2026-08-20 22:32:19 +0530

    Add Supabase client configuration

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ibkklujkizsszpwrviue.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_AXUglPvcbaMkOduQ6kWl4Q_ITVa-N1J";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
